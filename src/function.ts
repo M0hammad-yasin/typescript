975cbe045c688f16689f84c5421c34b80e112f67
M0hammad-yasin <mohdysn111@gmail.com>
2024-11-16 17:15:09 +0500

    function

---
+++ src/function.ts
@@ -0,0 +1,10 @@
+function subtraction(x: number, y: number): number {
+  if (x > y) {
+    return x - y;
+  }
+  return y - x;
+}
+
+function addition(x: number, y: number): number {
+  return x + y;
+}
