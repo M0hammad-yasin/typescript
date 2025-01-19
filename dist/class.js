4f1d634ec9cee6bdbd5452cf45cc8f628c12ff48
M0hammad-yasin <mohdysn111@gmail.com>
2025-01-19 11:50:23 +0500

    Refactor Account class to use private balance property with getter and setter

--- dist/class.js
+++ dist/class.js
@@ -3,19 +3,23 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0)
             return;
-        this.balance += amount;
+        this._balance += amount;
+    }
+    get balance() {
+        return this._balance;
+    }
+    set balance(value) {
+        if (value < 0)
+            return;
+        this._balance = value;
     }
 }
 let account = new Account(1, "John Doe", 1000);
-account.deposit(100);
 console.log(account.balance);
-account.deposit(-100);
+account.balance = 100;
 console.log(account.balance);
-if (account instanceof Account) {
-    console.log("account is an instance of Account");
-}
--- src/class.ts
+++ src/class.ts
@@ -1,23 +1,32 @@
 class Account {
-  id: number;
-  owner: string;
-  balance: number;
+  private readonly id: number;
+  private owner: string;
+  private _balance: number;
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = balance;
   }
   deposit(amount: number): void {
     if (amount <= 0) return;
-    this.balance += amount;
+    this._balance += amount;
+  }
+  get balance(): number {
+    return this._balance;
+  }
+  set balance(value: number) {
+    if (value < 0) return;
+    this._balance += value;
   }
 }
 let account = new Account(1, "John Doe", 1000);
-
-account.deposit(100);
-console.log(account.balance); // 1100
-account.deposit(-100);
-console.log(account.balance); // 1100
-if (account instanceof Account) {
-  console.log("account is an instance of Account");
-}
+console.log(account.balance);
+account.balance = 100;
+console.log(account.balance);
+// account.deposit(100);
+// console.log(account.balance); // 1100
+// account.deposit(-100);
+// console.log(account.balance); // 1100
+// if (account instanceof Account) {
+//   console.log("account is an instance of Account");
+// }
